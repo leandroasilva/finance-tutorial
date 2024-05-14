b12d515171609b5594a2dda3c5b6b8d2b8625ae8
leandroasilva <halk_lenson@hotmail.com>
2024-05-14 15:13:01 -0300

    feat: accounts get api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
-  const { data: accounts } = useGetAccounts();
   return (
     <div>
-      {accounts?.map((account) => (
-        <div key={account.id}>{account.name}</div>
-      ))}
+      home page
     </div>
   );
 }
