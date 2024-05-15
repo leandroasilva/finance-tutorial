c2d6db9dcd7fcd1ae67e3f1addcd2abeb7253b4c
leandroasilva <halk_lenson@hotmail.com>
2024-05-15 15:22:12 -0300

    feat: accounts POST api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
   return (
     <div>
-      home page
+      <Button onClick={onOpen}>Open</Button>
     </div>
   );
 }
