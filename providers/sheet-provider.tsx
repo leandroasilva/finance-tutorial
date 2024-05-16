d6e1ba12927df75b1470d1212aa53fdf331317f6
leandroasilva <halk_lenson@hotmail.com>
2024-05-16 18:47:00 -0300

    feat: Account edit

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -2,15 +2,17 @@
 
 import { useMountedState } from "react-use";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 
 export const SheetProvider = () => {
-    const isMounted = useMountedState();
+  const isMounted = useMountedState();
 
-    if(!isMounted) return null;
-    
-    return (
-        <>
-            <NewAccountSheet />
-        </>
-    )
-}
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <EditAccountSheet />
+      <NewAccountSheet />
+    </>
+  );
+};
