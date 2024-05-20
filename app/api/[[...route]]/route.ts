f6f7e3c8c8faff0b0dfad8cddf89bcde865d1a9c
leandroasilva <halk_lenson@hotmail.com>
2024-05-20 17:47:02 -0300

    feat: add transaction API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 
@@ -13,7 +14,8 @@ app.use("*", clerkMiddleware())
 
 const routes = app
     .route("/accounts", accounts)
-    .route("/categories", categories);
+    .route("/categories", categories)
+    .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
