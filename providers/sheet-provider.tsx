6be1e581d83aba25a273dfaf9e6aa16023429621
leandroasilva <halk_lenson@hotmail.com>
2024-05-22 17:35:35 -0300

    feat: transactions form

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -5,6 +5,8 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -17,6 +19,8 @@ export const SheetProvider = () => {
       <NewAccountSheet />
       <EditCategorySheet />
       <NewCategorySheet />
+      <EditTransactionSheet />
+      <NewTransactionSheet />
     </>
   );
 };
